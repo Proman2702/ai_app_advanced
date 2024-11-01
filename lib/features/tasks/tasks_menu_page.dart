cd81fcd2b31dd51219b8ecba1e6fac74cbc73839
Proman2702 <proman2702@gmail.com>
2024-11-01 14:26:10 +0300

    refactoring tasks done

--- lib/features/tasks/tasks_menu_page.dart
+++ lib/features/tasks/tasks_menu_page.dart
@@ -3,8 +3,11 @@
 import 'package:ai_app/etc/colors/colors.dart';
 import 'package:ai_app/etc/colors/gradients/background.dart';
 import 'package:ai_app/etc/colors/gradients/tiles.dart';
+import 'package:ai_app/features/diagnostics/diag_menu_tile.dart';
 import 'package:ai_app/features/drawer.dart';
 import 'package:ai_app/features/named_appbar.dart';
+import 'package:ai_app/features/tasks/tasks_menu_tile.dart';
+import 'package:ai_app/models/defects.dart';
 import 'package:ai_app/repositories/database/database_service.dart';
 import 'package:ai_app/repositories/database/get_values.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -24,6 +27,7 @@ class _TasksPageState extends State<TasksPage> {
   User? user;
   List<dynamic>? users;
   GetValues? dbGetter;
+  Map? defects;
 
   @override
   void initState() {
@@ -34,6 +38,21 @@ class _TasksPageState extends State<TasksPage> {
       setState(() {});
     });
     super.initState();
+  }
+
+  List<Widget> buildTiles() {
+    List<Widget> tiles = [];
+    Map defects = Defects.getAll();
+    defects.forEach((i, value) {
+      tiles.add(
+        TasksMenuTile(
+          dbGetter: dbGetter,
+          defectType: [i, value],
+        ),
+      );
+      tiles.add(const SizedBox(height: 15));
+    });
+    return tiles;
   }
 
   @override
@@ -110,146 +129,7 @@ class _TasksPageState extends State<TasksPage> {
                   const SizedBox(height: 50),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      // SizedBox(
-                      //   width: 330,
-                      //   child: Text(
-                      //     'Всего загружено 2 дефекта',
-                      //     style: TextStyle(color: Colors.white30, fontWeight: FontWeight.bold, fontSize: 12),
-                      //   ),
-                      // ),
-                      // SizedBox(
-                      //   width: 330,
-                      //   child: Text(
-                      //     'Нажмите на вкладку, чтобы пройти диагностику',
-                      //     style: TextStyle(color: Colors.white30, fontWeight: FontWeight.bold, fontSize: 12),
-                      //   ),
-                      // ),
-                      // SizedBox(height: 10),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.of(context).pushNamed('/tasks/levels', arguments: 1);
-                        },
-                        child: Container(
-                            width: 330,
-                            height: 80,
-                            alignment: Alignment.center,
-                            padding: const EdgeInsets.only(left: 15, right: 10),
-                            decoration: BoxDecoration(
-                                gradient: dbGetter?.getUser()?.defects == null
-                                    ? GreyTile()
-                                    : dbGetter!.getUser()!.defects['1'] == 2
-                                        ? TileGrad1()
-                                        : GreyTile(),
-                                borderRadius: BorderRadius.circular(20),
-                                boxShadow: const [
-                                  BoxShadow(spreadRadius: 1, offset: Offset(0, 3), blurRadius: 2, color: Colors.black26)
-                                ]),
-                            child: Column(
-                              children: [
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                const Row(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    Text('Коррекция картавости',
-                                        style: TextStyle(
-                                            fontSize: 20,
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.bold,
-                                            fontFamily: 'nunito'))
-                                  ],
-                                ),
-                                const SizedBox(height: 10),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  children: [
-                                    Text('Задание ${dbGetter?.getUser()?.current_level["1"] ?? 0}/6',
-                                        style: const TextStyle(
-                                            fontSize: 15, color: Colors.white, fontWeight: FontWeight.w600))
-                                  ],
-                                )
-                              ],
-                            )),
-                      ),
-                      const SizedBox(height: 5),
-                      SizedBox(
-                        width: 330,
-                        child: Text(
-                          dbGetter?.getUser()?.defects == null
-                              ? 'Загрузка...'
-                              : dbGetter!.getUser()!.defects['1'] == 1
-                                  ? 'Не рекомендуется диагностикой'
-                                  : dbGetter!.getUser()!.defects['1'] == 2
-                                      ? 'Рекомендуется'
-                                      : 'Диагностика не пройдена',
-                          style: const TextStyle(color: Colors.white30, fontWeight: FontWeight.bold, fontSize: 12),
-                        ),
-                      ),
-                      const SizedBox(height: 15),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.of(context).pushNamed('/tasks/levels', arguments: 2);
-                        },
-                        child: Container(
-                            width: 330,
-                            height: 80,
-                            alignment: Alignment.center,
-                            padding: const EdgeInsets.only(left: 15, right: 10),
-                            decoration: BoxDecoration(
-                                gradient: dbGetter?.getUser()?.defects == null
-                                    ? GreyTile()
-                                    : dbGetter!.getUser()!.defects['2'] == 2
-                                        ? TileGrad1()
-                                        : GreyTile(),
-                                borderRadius: BorderRadius.circular(20),
-                                boxShadow: const [
-                                  BoxShadow(spreadRadius: 1, offset: Offset(0, 3), blurRadius: 2, color: Colors.black26)
-                                ]),
-                            child: Column(
-                              children: [
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                const Row(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    Text('Коррекция "Г"',
-                                        style: TextStyle(
-                                            fontSize: 20,
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.bold,
-                                            fontFamily: 'nunito'))
-                                  ],
-                                ),
-                                const SizedBox(height: 10),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  children: [
-                                    Text('Задание ${dbGetter?.getUser()?.current_level["2"] ?? 0}/6',
-                                        style: const TextStyle(
-                                            fontSize: 15, color: Colors.white, fontWeight: FontWeight.w600))
-                                  ],
-                                )
-                              ],
-                            )),
-                      ),
-                      const SizedBox(height: 5),
-                      SizedBox(
-                        width: 330,
-                        child: Text(
-                          dbGetter?.getUser()?.defects == null
-                              ? 'Загрузка...'
-                              : dbGetter!.getUser()!.defects['2'] == 1
-                                  ? 'Не рекомендуется диагностикой'
-                                  : dbGetter!.getUser()!.defects['2'] == 2
-                                      ? 'Рекомендуется'
-                                      : 'Диагностика не пройдена',
-                          style: const TextStyle(color: Colors.white30, fontWeight: FontWeight.bold, fontSize: 12),
-                        ),
-                      ),
-                    ],
+                    children: buildTiles(),
                   ),
                   const SizedBox(height: 80),
                 ],
---
+++ lib/features/tasks/tasks_menu_tile.dart
@@ -0,0 +1,74 @@
+import 'package:ai_app/etc/colors/gradients/tiles.dart';
+import 'package:ai_app/repositories/database/get_values.dart';
+import 'package:flutter/material.dart';
+
+class TasksMenuTile extends StatelessWidget {
+  const TasksMenuTile({super.key, required this.dbGetter, required this.defectType});
+
+  final GetValues? dbGetter;
+  final List defectType;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed('/tasks/levels', arguments: int.parse(defectType[0]));
+          },
+          child: Container(
+              width: 330,
+              height: 80,
+              alignment: Alignment.center,
+              padding: const EdgeInsets.only(left: 15, right: 10),
+              decoration: BoxDecoration(
+                  gradient: dbGetter?.getUser()?.defects == null
+                      ? GreyTile()
+                      : dbGetter!.getUser()!.defects[defectType[0]] == 2
+                          ? TileGrad1()
+                          : GreyTile(),
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: const [
+                    BoxShadow(spreadRadius: 1, offset: Offset(0, 3), blurRadius: 2, color: Colors.black26)
+                  ]),
+              child: Column(
+                children: [
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text('${defectType[1]}',
+                          style: const TextStyle(
+                              fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold, fontFamily: 'nunito'))
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text('Задание ${dbGetter?.getUser()?.current_level[defectType[0]] ?? 0}/6',
+                          style: const TextStyle(fontSize: 15, color: Colors.white, fontWeight: FontWeight.w600))
+                    ],
+                  )
+                ],
+              )),
+        ),
+        const SizedBox(height: 5),
+        SizedBox(
+          width: 330,
+          child: Text(
+            dbGetter?.getUser()?.defects == null
+                ? 'Загрузка...'
+                : dbGetter!.getUser()!.defects[defectType[0]] == 1
+                    ? 'Не рекомендуется диагностикой'
+                    : dbGetter!.getUser()!.defects[defectType[0]] == 2
+                        ? 'Рекомендуется'
+                        : 'Диагностика не пройдена',
+            style: const TextStyle(color: Colors.white30, fontWeight: FontWeight.bold, fontSize: 12),
+          ),
+        ),
+      ],
+    );
+  }
+}
