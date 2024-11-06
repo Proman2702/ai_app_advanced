5efaf875637a4bdf3d8a2bf06cb1b1c6991b3cb5
Proman2702 <proman2702@gmail.com>
2024-11-06 08:30:50 +0300

    parsing fix

--- youtube_parse/main.py
+++ youtube_parse/main.py
@@ -1,14 +1,42 @@
+import random
+import string
 import yt_dlp
+from mutagen.mp3 import MP3
+import os
 
-ydl_opts = {
+f = "".join(open("videos.txt").readlines()).splitlines()
+
+
+
+
+for i in f:
+    with yt_dlp.YoutubeDL({
+    'outtmpl': f'output/{"".join(random.choice(string.ascii_letters) for _ in range(16))}.%(ext)s',
     'format': 'bestaudio/best',
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
-        'preferredquality': '64',
+        'preferredquality': '8',
     }],
-}
+}) as ydl:
+        ydl.download(i)
 
 
-with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(['https://youtube.com/watch?v=2lAe1cqCOXo'])
+def convert_seconds(seconds):
+    hours = seconds // 3600
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+    return "%02d:%02d:%02d" % (hours, minutes, seconds)
+
+path = "output/"
+total_length = 0
+for root, dirs, files in os.walk(os.path.abspath(path)):
+    for file in files:
+        if file.endswith(".mp3"):
+            audio = MP3(os.path.join(root, file))
+            length = audio.info.length
+            total_length += length
+
+hours, minutes, seconds = convert_seconds(total_length).split(":")
+print("total duration: " + str(int(hours)) + ':' + str(int(minutes)) + ':' + str(int(seconds)))
